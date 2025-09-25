24070ec38ec576c24b9db1f06073b8e83c95b399
Tomás Hernández <tom.jes23@gmail.com>
2025-09-25 17:43:19 -0300

    Endpoints de prueba para servir libros

--- app/api/lector/routes.py
+++ app/api/lector/routes.py
@@ -1,11 +1,48 @@
-from flask import Blueprint, request, send_file, Response
+from flask import Blueprint, request, send_file, Response, jsonify
 import os
-from app import Config
+from app.config import Config
 lector_bp = Blueprint('lector', __name__)
 
-@lector_bp.route('/libro/test/', methods=['GET'])
-def get_page():
-    ruta = Config.PDF_UPLOAD_FOLDER + "/AJEDREZ-ELEMENTOS-DE-TACTICA.pdf"
+
+@lector_bp.route('/libro/<int:id_libro>/total_paginas', methods=['GET'])
+def get_total_pages(id_libro):
+    ruta_pdf = os.path.join(Config.IMAGE_CACHE_FOLDER, str(id_libro))
+
+    if not os.path.exists(ruta_pdf):
+        return jsonify({"error": "El libro no existe"}), 404
+    
+    try:
+        todos_los_archivos = os.listdir(ruta_pdf)
+        total_paginas = len(todos_los_archivos)
+        return jsonify({"total_paginas": total_paginas}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@lector_bp.route('/libro/<int:id_libro>/<int:page_num>', methods=['GET'])
+def get_page(id_libro, page_num):
+    ruta = os.path.join(Config.IMAGE_CACHE_FOLDER, str(id_libro))
+    imagen_path = os.path.join(ruta, f"pagina-{page_num}.jpg")
+
+    if os.path.exists(imagen_path):
+        return send_file(imagen_path, mimetype='image/jpeg')
+    else:
+        return Response("Página no encontrada", status=404)
+    
+
+
+@lector_bp.route('/libro/<int:id_libro>', methods=['GET'])
+def get_libro(id_libro):
+    if id_libro == 1:
+        ruta = os.path.join(Config.PDF_UPLOAD_FOLDER, "AJEDREZ-ELEMENTOS-DE-TACTICA.pdf")
+    elif id_libro == 2:
+        ruta = os.path.join(Config.PDF_UPLOAD_FOLDER, "1984.pdf")
+    elif id_libro == 3:
+        ruta = os.path.join(Config.PDF_UPLOAD_FOLDER, "LLL.pdf")
+    else:
+        return {"error": "Libro no encontrado"}, 404
+
     rango = request.headers.get('Range', None)
 
     if not rango:  # descarga completa si no hay Range
